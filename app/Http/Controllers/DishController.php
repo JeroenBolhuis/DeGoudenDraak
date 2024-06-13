fdffc4c5b383a2cc9c2c8876dcc68d0c1268e3c6
Sebastiaantjuh <sebastiaanb598@gmail.com>
2024-06-13 19:49:34 +0200

    show, delete & create methods created

---
+++ app/Http/Controllers/DishController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Dish;
+use Illuminate\Http\Request;
+
+class DishController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $dishes = Dish::all();
+        return view('dish.index', ['dishes' => $dishes]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('dish.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([ 
+            'dishnumber' => 'nullable|numeric',
+            'addition' => 'nullable|alpha',
+            'name' => 'required|string',
+            'price' => 'required|numeric',
+            'description' => 'required|string',
+            'dishtype' => 'required|exists:dishtype,type'
+        ]);
+
+        $dish = new Dish();
+        $dish->dishnumber = $validatedData['dishnumber'];
+        $dish->addition = $validatedData['addition'];
+        $dish->name = $validatedData['name'];
+        $dish->price = $validatedData['price'];
+        $dish->description = $validatedData['description'];
+        $dish->dishtype = $validatedData['dishtype'];
+        $dish->save();
+
+        return redirect()->route('dish.index')->with('dishes', $dish->toArray());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $dish = Dish::find($id);
+        
+        if(!$dish) {
+            return redirect()->route(404);
+        }
+
+        return view('dish.show', ['dish' => $dish]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $dish = Dish::find($id);
+        return view('dish.edit', ['dish' => $dish]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $dish = Dish::find($id);
+        $dish->delete();
+
+        return redirect()->route('dish.index');
+    }
+}
